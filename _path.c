1bb2f60d46f906b321618a540efdeecfbc95c3e1
root <Nelsonmwaura>
2023-04-26 06:00:42 -0700

    corrections

--- _path.c
+++ _path.c
@@ -8,16 +8,15 @@
 
 char **_path(char **environ)
 {
-	int location = 0;
-	char **token;
+int location = 0;
+char **token;
 
-	for (; environ[location] != NULL ; location++)
-	{
-		if (environ[location][0] == 'P && environ[location][2] == 'T')
-		{
-			token = &(environ[location]);
-		}
-	}
-
-	return (token);
+for (; environ[location] != NULL ; location++)
+{
+if (environ[location][0] == 'P && environ[location][2] == 'T')
+{
+token = &(environ[location]);
+}
+}
+return (token);
 }
