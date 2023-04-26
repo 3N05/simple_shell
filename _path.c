3412eb1561bcdfd263bc7cb69e83d521168f951c
root <Nelsonmwaura>
2023-04-26 05:42:08 -0700

    path and exit

---
+++ _path.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+
+/**
+ * _path - path to execute the commands
+ * @environ: the environ variable
+ * Return: returns token
+ */
+
+char **_path(char **environ)
+{
+	int locate = 0;
+	char **token;
+
+	for (; environ[locate] != NULL ; locate++)
+	{
+		if (environ[locate][0] == 'p' && environ[locate][2] == 'T')
+		{
+			token = &(environ[locae]);
+		}
+	}
+	return (token);
+}
