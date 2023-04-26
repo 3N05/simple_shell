d9bd74aa7679f0e66e69afa443be749cae44b2e3
root <Nelsonmwaura>
2023-04-26 10:40:05 -0700

    exe.c

---
+++ exe.c
@@ -0,0 +1,55 @@
+#include "shell.h"
+
+/**
+ * _exe - executes a program exe
+ * *@line: receives from strtok the token
+ * @argv: arguments in file
+ * @num: variables
+ * @isatty_val: it is the tty
+ * @env: enviroment variable
+ * Return: nothing
+ */
+
+void _exe(char **line, char *argv, int num, int isatty_val, char **env)
+{
+pid_t pid;
+int _exec = 0;
+
+if (line[0] == NULL)
+return;
+
+if (is_built(line, env) == 1)
+return;
+
+pid = fork();
+if (pid < 0)
+{
+printf("Error during fork\n");
+free_dp(line);
+exit(EXIT_FAILURE);
+}
+else if (pid != 0)
+{
+wait(NULL);
+return;
+}
+else
+{
+if (check_path(env, line) == 0)
+_exec = exe(line[0], line, NULL);
+if (_exec < 0)
+{
+if (isatty_val == 1)
+{
+printf("%s: No such file or directory\n", argv);
+free(line);
+exit(EXIT_SUCCESS);
+}
+printf("%s: %d: %s: not found\n", argv, num, line[0]);
+free(line);
+exit(EXIT_FAILURE);
+}
+free_dp(line);
+exit(EXIT_SUCCESS);
+}
+}
